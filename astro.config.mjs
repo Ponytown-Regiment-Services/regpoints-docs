0d9a5c72fd1694231f5ef4630110157bd4376569
betawolfy <contact.betawolfy@gmail.com>
2026-02-10 15:45:45 +0100

    WIP - Continued adding content to the guide. - Manage regiment tab (info and points tab in details) - intro of the project. - PRS advert.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,45 +3,47 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import Icons from 'unplugin-icons/vite'
 import starlightThemeObsidian from 'starlight-theme-obsidian'
-import starlightSiteGraph from 'starlight-site-graph'
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          plugins: [starlightThemeObsidian(), starlightSiteGraph({
-              overridePageSidebar: false
-          })],
-          title: 'Regpoints Docs',
-          // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      { label: 'Introduction', slug: 'guides/introduction' },
-                      { label: 'Getting Started', slug: 'guides/getting-started' },
-                      { label: 'Manage Regiment', slug: 'guides/manage-regiment' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-              {
+    integrations: [
+        starlight({
+            plugins: [
+                starlightThemeObsidian({
+                   backlinks: false,
+                   graph: false
+            }),
+            ],
+            title: 'Regpoints Docs',
+            // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        { label: 'Introduction', slug: 'guides/introduction' },
+                        { label: 'Getting Started', slug: 'guides/getting-started' },
+                        { label: 'Manage Regiment', slug: 'guides/manage-regiment' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
                     label: 'Others',
                     items: [
                         { label: 'Changelog', slug: 'changelog' },
                     ],
-              }
-          ],
-      }),
-	],
+                }
+            ],
+        }),
+    ],
 
-  vite: {
-      plugins: [Icons({ compiler: 'astro' })],
-	},
+    vite: {
+        plugins: [Icons({ compiler: 'astro' })],
+    },
 
-  adapter: vercel(),
+    adapter: vercel(),
 });
